83debb2f2e9abed4deef6a7dbc2d848cf19abcd7
Erdkash1 <erdkash1@gmail.com>
2026-03-25 15:24:41 -0500

    Add DTO(Data Transfer Object) and updated pom.xml

---
+++ src/main/java/com/iggy/ecommerce/dto/AuthResponse.java
@@ -0,0 +1,8 @@
+package com.iggy.ecommerce.dto;
+
+public class AuthResponse {
+    private String token;
+
+    public String getToken() {return token;}
+    public void setToken(String token) {this.token = token;}
+}
