50be9dfcbd7fd21bffe0fe33759909ed2c599361
Erdkash1 <erdkash1@gmail.com>
2026-03-25 23:40:34 -0500

    add AuthService with register and login, and AuthResponse updated

--- src/main/java/com/iggy/ecommerce/dto/AuthResponse.java
+++ src/main/java/com/iggy/ecommerce/dto/AuthResponse.java
@@ -5,4 +5,8 @@ public class AuthResponse {
 
     public String getToken() {return token;}
     public void setToken(String token) {this.token = token;}
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
 }
