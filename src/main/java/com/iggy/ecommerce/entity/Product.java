489876644cb7e661e853caacb89c8fa1bb010631
Erdkash1 <erdkash1@gmail.com>
2026-05-26 23:31:47 -0500

    feat: add Redis caching for product endpoints

--- src/main/java/com/iggy/ecommerce/entity/Product.java
+++ src/main/java/com/iggy/ecommerce/entity/Product.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "products")
-public class Product {
+public class Product implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
